e6b0d39628a2c357443031cabb4b9c8192dc9094
Diana Marysabell Llamoca Zárate <dianallamocaz@gmail.com>
2025-10-16 18:33:03 -0500

    Update losses_metrics.py

--- losses_metrics.py
+++ losses_metrics.py
@@ -1,5 +1,7 @@
 #Aquí se van a crear las losses y métricas para entrenar y evaluar el modelo :)
+import torch
 
+#LOSSES:
 #BCELoss
 bce_loss=torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor([8]).to(device)) #Coloco un WEIGHT más alto para la imbalanced class
 
@@ -48,3 +50,49 @@ def focal_tversky_loss(pred, target, alpha=0.3, beta=0.7, gamma=0.75, smooth=1):
     return focal_tversky.mean()
 
     return loss_final
+
+
+#MÉTRICAS:
+
+#Precision:
+def precision(mask_pred_log,mask_org,threshold):
+  #Convierto la máscara predicha con logits a máscaras con píxeles de 0-1
+  sm=torch.nn.Sigmoid()
+  mask_pred_probs=sm(mask_pred_log)
+  
+  #Máscara predicha binarizada (0 y 1) de acuerdo al threshold
+  mask_pred_th=(mask_pred_probs>threshold).float()
+
+  #Calculo la cantidad de verdaderos positivos en la imagen: INTERSECCIÓN
+  t_p=(mask_pred_th*mask_org).sum()
+  
+  #Calculo TP+FP
+  tp_fp=(mask_pred_th).sum()
+  
+  #Precision
+  precision=t_p/tp_fp
+
+  return precision
+
+
+#Recall:
+def recall(mask_pred_log,mask_org,threshold):
+  #Sigmoid
+  sgm=torch.nn.Sigmoid()
+
+  #Máscara: logs -> probs
+  mask_pred_probs=sgm(mask_pred_log)
+
+  #Máscara threshold
+  mask_pred_th=(mask_pred_probs>threshold).float()
+
+  #True positive
+  tp=(mask_pred_th*mask_org).sum()
+
+  #TP+FN
+  tp_fn=(mask_org).sum()
+
+  #Recall
+  recall=tp/tp_fn
+
+  return recall
